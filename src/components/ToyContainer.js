2eba6c8a4c0697185ad8fc24271449e4f961b601
chris <cseiter@gmail.com>
2021-06-06 21:30:37 -0400

    toys rendered on page

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
-  return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
-  );
+function ToyContainer({toys}) {
+  const toyCards = toys.map((toy) => (
+    <ToyCard
+      key={toy.id}
+      toy={toy}
+    />
+  ));
+
+  return <div id="toy-collection">{toyCards}</div>;
 }
 
 export default ToyContainer;
